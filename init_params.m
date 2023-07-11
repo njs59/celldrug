b87f1c24f49a9e4ed0861d0d1202f2a3489c8e25
Nathan Joel Schofield <schofieldn@maths.ox.ac.uk>
2023-07-11 17:12:02 +0100

    Rewritten to only have a single carrying capacity

--- init_params.m
+++ init_params.m
@@ -10,19 +10,25 @@ param_out = zeros(no_params,1);
 grid_size = 10;
 l = grid_size;
 
+K = l^2;
+K1 = K;
+K2 = K;
+K3 = K;
+K4 = K;
+
 r1 = 0.027;
 param_out(1) = r1;
 
 alpha12 = 2;
 param_out(2) = alpha12;
 
-K1 = l^2;
+%K1 = l^2;
 param_out(3) = K1;
-K2 = l^2;
+%K2 = l^2;
 param_out(4) = K2;
 
 if drug_present == 1
-    d_drug = 0.022;
+    d_drug = 0.024;
     d1 = d_drug;
 elseif drug_present == 0
     d1 = 0;
@@ -34,12 +40,12 @@ resistance_cost = 0.5;
 r2 = r1*(1-resistance_cost);
 param_out(6) = r2;
 
-alpha21 = 2;
+alpha21 = 0.1;
 param_out(7) = alpha21;
 
-K3 = l^2;
+%K3 = l^2;
 param_out(8) = K3;
-K4 = l^2;
+%K4 = l^2;
 param_out(9) = K4;
 
 if drug_present == 1 && res_drug_death
@@ -61,8 +67,8 @@ end
 delta1 = 1 - d1/r1;
 delta2 = 1 - d2/r2;
 
-c1_hat = (1/alpha12)*delta1 - delta2;
-c2_hat = delta1 - (1/alpha21)*delta2;
+c1_hat = delta1 - delta2*(1/alpha21);
+c2_hat = delta1*(1/alpha12) - delta2;
 
 disp('c hat values')
 disp(c1_hat)
